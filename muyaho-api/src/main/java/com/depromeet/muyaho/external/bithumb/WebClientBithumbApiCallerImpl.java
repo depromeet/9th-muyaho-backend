3f4eb047b41ae62074efff75e7fd190c588aba4d
seungh0 <will.seungho@gmail.com>
2021-04-17 19:23:19 +0900

    feat: 빗썸 종목코드를 통해 현재가를 조회해오는 외부 API 연동 (차후 내부적으로 사용할)

---
+++ muyaho-api/src/main/java/com/depromeet/muyaho/external/bithumb/WebClientBithumbApiCallerImpl.java
@@ -0,0 +1,26 @@
+package com.depromeet.muyaho.external.bithumb;
+
+import com.depromeet.muyaho.external.bithumb.dto.response.BithumbTradeInfoResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@Component
+public class WebClientBithumbApiCallerImpl implements BithumbApiCaller {
+
+    private final WebClient webClient;
+
+    @Override
+    public BithumbTradeInfoResponse retrieveTrades(String marketCode) {
+        return webClient.get()
+            .uri("https://api.bithumb.com/public/ticker/" + marketCode)
+            .retrieve()
+            .onStatus(HttpStatus::isError, errorResponse -> Mono.error(new IllegalArgumentException("빗썸 외부 API 연동 중 에러가 발생하였습니다")))
+            .bodyToMono(BithumbTradeInfoResponse.class)
+            .block();
+    }
+
+}
---
+++ muyaho-api/src/main/java/com/depromeet/muyaho/external/bithumb/dto/response/BithumbTradeDataResponse.java
@@ -0,0 +1,29 @@
+package com.depromeet.muyaho.external.bithumb.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+class BithumbTradeDataResponse {
+
+    private int minPrice;
+
+    private int maxPrice;
+
+    private int openingPrice;
+
+    private int closingPrice;
+
+    @Builder
+    public BithumbTradeDataResponse(int minPrice, int maxPrice, int openingPrice, int closingPrice) {
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+        this.openingPrice = openingPrice;
+        this.closingPrice = closingPrice;
+    }
+
+}
