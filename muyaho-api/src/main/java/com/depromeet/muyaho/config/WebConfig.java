265ea46f9646ca48f72ac6f2429788b4ea609e55
seungh0 <will.seungho@gmail.com>
2021-04-19 17:42:29 +0900

    feat: CORS 설정

---
+++ muyaho-api/src/main/java/com/depromeet/muyaho/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.depromeet.muyaho.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS");
+    }
+
+}
