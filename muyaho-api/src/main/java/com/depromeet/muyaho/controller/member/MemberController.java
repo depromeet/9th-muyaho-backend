8f3a902e87d76f7e377cf4b01726eea2602dc366
Seungho Kang <will.seungho@gmail.com>
2021-05-05 15:09:43 +0900

    스웨거 Authoirzation 헤더 나오게 수정

--- muyaho-api/src/main/java/com/depromeet/muyaho/controller/member/MemberController.java
+++ muyaho-api/src/main/java/com/depromeet/muyaho/controller/member/MemberController.java
@@ -7,6 +7,7 @@ import com.depromeet.muyaho.service.member.MemberService;
 import com.depromeet.muyaho.service.member.dto.request.UpdateMemberRequest;
 import com.depromeet.muyaho.service.member.dto.response.MemberInfoResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -19,21 +20,22 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @Operation(summary = "나의 회원 정보를 조회하는 API", description = "Authorization 헤더가 필요합니다", security = {@SecurityRequirement(name = "bearer-key")})
+
+    @Operation(summary = "나의 회원 정보를 조회하는 API", security = {@SecurityRequirement(name = "Authorization")}, parameters = @Parameter(name = "Authorization"))
     @Auth
     @GetMapping("/api/v1/member")
     public ApiResponse<MemberInfoResponse> getMyMemberInfo(@MemberId Long memberId) {
         return ApiResponse.success(memberService.getMemberInfo(memberId));
     }
 
-    @Operation(summary = "나의 회원 정보를 수정하는 API", description = "Authorization 헤더가 필요합니다", security = {@SecurityRequirement(name = "bearer-key")})
+    @Operation(summary = "나의 회원 정보를 수정하는 API", security = {@SecurityRequirement(name = "Authorization")}, parameters = @Parameter(name = "Authorization"))
     @Auth
     @PutMapping("/api/v1/member")
     public ApiResponse<MemberInfoResponse> updateMemberInfo(@Valid @RequestBody UpdateMemberRequest request, @MemberId Long memberId) {
         return ApiResponse.success(memberService.updateMemberInfo(request, memberId));
     }
 
-    @Operation(summary = "회원탈퇴 API", description = "Authorization 헤더가 필요합니다", security = {@SecurityRequirement(name = "bearer-key")})
+    @Operation(summary = "회원탈퇴 API", security = {@SecurityRequirement(name = "Authorization")}, parameters = @Parameter(name = "Authorization"))
     @Auth
     @DeleteMapping("/api/v1/member")
     public ApiResponse<String> deleteMemberInfo(@MemberId Long memberId) {
