926a69e8882b269f23689424703d47e4d285838e
Seungho Kang <will.seungho@gmail.com>
2021-06-03 04:10:13 +0900

    [MUYAHO-32] StockPriceChange 도메인 삭제

--- muyaho-domain/src/main/java/com/depromeet/muyaho/domain/domain/stockchange/StockPriceChangeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.depromeet.muyaho.domain.domain.stockchange;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockPriceChangeRepository extends JpaRepository<StockPriceChange, Long> {
-
-}
