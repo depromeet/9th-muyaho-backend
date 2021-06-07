9a460ecbeac532c655ba0dd95923c65284a87f0d
Seungho Kang <will.seungho@gmail.com>
2021-06-07 14:55:23 +0900

    [MH-44] logback 설정 수정.

--- muyaho-api/src/main/java/com/depromeet/muyaho/api/controller/MainController.java
+++ muyaho-api/src/main/java/com/depromeet/muyaho/api/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.depromeet.muyaho.api.controller;
 
-import com.depromeet.muyaho.api.config.aop.BenchMark;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @Operation(summary = "Health Check")
-    @BenchMark
     @GetMapping({"/", "/ping"})
     public ApiResponse<String> ping() {
         return ApiResponse.success("pong");
