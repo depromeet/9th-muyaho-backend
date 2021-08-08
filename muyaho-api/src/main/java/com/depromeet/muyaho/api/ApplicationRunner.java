66733796dd5a1882d09568dcdbf918227f668d7e
seungh0 <will.seungho@gmail.com>
2021-08-09 08:51:31 +0900

    [MH-78] 서버가 새로 시작하면, 주식 정보를 캐싱하도록 구성

--- muyaho-api/src/main/java/com/depromeet/muyaho/api/ApplicationRunner.java
+++ muyaho-api/src/main/java/com/depromeet/muyaho/api/ApplicationRunner.java
@@ -1,6 +1,8 @@
 package com.depromeet.muyaho.api;
 
 import com.depromeet.muyaho.common.utils.LocalDateTimeUtils;
+import com.depromeet.muyaho.domain.domain.stock.StockMarketType;
+import com.depromeet.muyaho.domain.domain.stock.StockRepository;
 import com.depromeet.muyaho.domain.event.notification.ApplicationEventOccurredEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
@@ -14,10 +16,21 @@ import org.springframework.stereotype.Component;
 public class ApplicationRunner implements CommandLineRunner, ApplicationListener<ContextClosedEvent> {
 
     private final ApplicationEventPublisher eventPublisher;
+    private final StockRepository stockRepository;
 
     @Override
     public void run(String... args) {
         eventPublisher.publishEvent(ApplicationEventOccurredEvent.of("API 서버가 시작됩니다", LocalDateTimeUtils.now()));
+        cachingActiveStocks();
+    }
+
+    /**
+     * 서버가 동작하면 주식 정보들을 캐싱해준다.
+     */
+    private void cachingActiveStocks() {
+        for (StockMarketType type : StockMarketType.values()) {
+            stockRepository.findAllActiveStockByType(type);
+        }
     }
 
     // 단, SIGKILL시 이벤트가 호출되지 않음.
